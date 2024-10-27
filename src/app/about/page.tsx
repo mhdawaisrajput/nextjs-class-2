fde70afe3b3554a5b777d499b9aa98ac83a0058e
Muhammad Awais <162321104+mhdawaisrajput@users.noreply.github.com>
2024-10-27 08:27:10 -0700

    class 2 code updated

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <>
+      <h1>This is My About Page:</h1>
+    </>
+  );
+}
+
+/*  import Link from "next/link";
+export default function About() {
+  return (
+    <>
+      <Link href="/">Home:</Link>
+      <Link href="/about">About:</Link>
+      <Link href="/contact">Contact:</Link>
+      <h1>This is My About Page:</h1>
+    </>
+  );
+}
+
+function Aboutpage() {
+  return (
+    <>
+      <a href="/">Home:</a>
+      <a href="/about">About:</a>
+      <a href="/contact">Contact:</a>
+      <h1>This is My About Page:</h1>
+    </>
+  );
+}  */
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,30 @@
+export default function Contact() {
+  return (
+    <>
+      <h1>This is My Contact Page:</h1>
+    </>
+  );
+}
+
+/*  import Link from "next/link";
+export default function Contact() {
+  return (
+    <>
+      <Link href="/">Home:</Link>
+      <Link href="/about">About:</Link>
+      <Link href="/contact">Contact:</Link>
+      <h1>This is My Contact Page:</h1>
+    </>
+  );
+}
+
+function Contactpage() {
+  return (
+    <>
+      <a href="/">Home:</a>
+      <a href="/about">About:</a>
+      <a href="/contact">Contact:</a>
+      <h1>This is My Contact Page:</h1>
+    </>
+  );
+}  */
